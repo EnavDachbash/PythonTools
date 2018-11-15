e236b51565b1091bb3d8bfc4c93a4f240a72eb0e
enav.hidekel <enav.hidekel@myheritage.com>
2018-11-15 13:27:12 +0200

    Added a new directory for aws scripts + new file for current project

---
+++ aws_cli/push_bucket_poloicy.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+__author__ = 'Enav Hidekel'
+__date__ = 'Nov 15, 2018'
+
+"""
+This script will
+1. receive 1 account number
+2. list all buckets
+3. for each bucket >>
+  3.1 change policy file
+  3.2 update policy
+"""
+
+account = input('Enter Desired Account Number: ')
