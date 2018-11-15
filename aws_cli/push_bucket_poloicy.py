acdae1007246576198104f16914775eea07b3839
enav.hidekel <enav.hidekel@myheritage.com>
2018-11-15 16:13:55 +0200

    Added retrieval of admin role per account

--- aws_cli/push_bucket_poloicy.py
+++ aws_cli/push_bucket_poloicy.py
@@ -12,6 +12,8 @@ This script will
   3.2 update policy
 """
 
+import boto3
+import re
 from argparse import ArgumentParser
 
 # Set up argument\s parsing:
@@ -26,6 +28,34 @@ if not getattr(args, 'account'):
 
 # Handle multiple accounts input:
 accounts = args.account.split(',')
+
+
+def get_admin_role(a):
+    """ This function would receive account number and list all of it's s3 buckets. """
+    # Iterate over each account provided, and list buckets
+    with open('/home/enav.hidekel/.aws/config', 'r') as current:
+        prev_line = ""
+        line_number = 1
+        global role
+        role = ""
+        for line in current.readlines():
+            if re.search(a, line):
+                role = prev_line.split()[1][:-1]
+            prev_line = line
+            line_number += 1
+
+
+def main():
+
+    for a in accounts:
+        get_admin_role(a)
+
+    print(role)
+#    s3 = boto3.resource('s3')
+
+
+if __name__ == '__main__':
+    main()
 
 
 
