3c141185b3912c72ed6e1820616fc154b4395f10
enav.hidekel <enav.hidekel@myheritage.com>
2019-02-05 11:43:54 +0200

    Description: Added the diferentiate between versions to the listing function of different mounts so it supports v2 paths as well.

--- Vault_Hashicorp/listSecretsAtMount.py
+++ Vault_Hashicorp/listSecretsAtMount.py
@@ -20,6 +20,16 @@ def get_token():
         return token
 
 
+def get_mount_version(source_mount):
+    secret_backend_config=client.sys.read_mount_configuration(source_mount)
+    if 'options' in secret_backend_config.keys():
+        print('mount is v2')
+        secret_version='v2'
+    else:
+        secret_version='v1'
+        print('mount is v1')
+    return secret_version
+
 # Initialize the client using TLS
 os.environ["VAULT_TOKEN"] = get_token()
 client = hvac.Client(url='https://vault.dal.myhrtg.net:8200', token=os.environ["VAULT_TOKEN"], verify=False)
@@ -27,20 +37,27 @@ client = hvac.Client(url='https://vault.dal.myhrtg.net:8200', token=os.environ["
 # ================== END GLOBAL ================== #
 
 
-# This works
-def list_secrets_in_path(path):
-    objects_in_path = client.secrets.kv.v1.list_secrets(path=path)['data']['keys']
+# This function will list all secrets in mount
+def list_secrets_in_path(mount_version, source_mount, dir_in_mount, final_secrets_list):
+    if '1' in mount_version:
+        objects_in_path = client.secrets.kv.v1.list_secrets(path=dir_in_mount, mount_point=source_mount)['data']['keys']
+    else:
+        objects_in_path = client.secrets.kv.v2.list_secrets(path=dir_in_mount, mount_point=source_mount)['data']['keys']
     for object in objects_in_path:
-        abs_path = '{}/{}'.format(path, object)
+        abs_path = '{}/{}'.format(dir_in_mount, object)
         if object.endswith('/'):
-            list_secrets_in_path(abs_path.strip('/'))
+            list_secrets_in_path(mount_version, source_mount, abs_path.strip('/'), final_secrets_list)
         else:
-            print(abs_path)
+            final_secrets_list.append('{}/{}'.format(source_mount, abs_path.lstrip(('/'))))
+    return final_secrets_list
 
 
 def main():
-    source_mount = input("Enter the source mount, and omit the /secret prefix:   ")
-    list_secrets_in_path(source_mount)
+    source_mount = input("Enter the mount you wish to list: ")
+    mount_version = get_mount_version(source_mount)
+    final_secrets_list = list()
+    original_mount_secrets = list_secrets_in_path(mount_version, source_mount, '', final_secrets_list)
+    print(original_mount_secrets)
 
 
 if __name__ == '__main__':
