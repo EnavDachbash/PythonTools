91eb79304b188702ea165748f8fe44355adf721d
enav.hidekel <enav.hidekel@myheritage.com>
2019-02-05 12:43:11 +0200

    Description: Excluded all parts that doesn't relate to the set of the env var.

--- Vault_Hashicorp/getToken.py
+++ Vault_Hashicorp/getToken.py
@@ -1,10 +1,13 @@
+#!/usr/bin/env python3
+__author__ = 'Enav Hidekel <enav.hidekel@gmail.com>'
+__creation_date__ = '01/02/19'
+
+""" Be sure to log into vault before running this script! 
+    This script will retreive the user's token and set it to environment
+    variable called 'VAULT_TOKEN'. """
+
 import os
 import sys
-import hvac
-import urllib3
-
-urllib3.disable_warnings()
-
 
 def get_token():
     try:
@@ -20,7 +23,6 @@ def get_token():
 
 # Initialize the client using TLS + env Var
 os.environ["VAULT_TOKEN"] = get_token()
-client = hvac.Client(url='https://vault.dal.myhrtg.net:8200', token=os.environ["VAULT_TOKEN"], verify=False)
 
 # Verify the token is set as env var
 print('env var set successful: {}'.format(os.environ["VAULT_TOKEN"]))
