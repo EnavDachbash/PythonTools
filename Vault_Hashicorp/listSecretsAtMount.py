5328197b778fbafd41778c1233a0413036b8f6f9
enav.hidekel <enav.hidekel@myheritage.com>
2019-02-05 12:28:49 +0200

    Description: Removed redundant packages imported, fixed output for script.

--- Vault_Hashicorp/listSecretsAtMount.py
+++ Vault_Hashicorp/listSecretsAtMount.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
-""" Be sure to log into vault before running this script """
+
+__author__ = 'Enav Hidekel <enav.hidekel@gmail.com>'
+__creation_date__ = '31/01/19'
+
+""" Be sure to log into vault before running this script!
+    This script will list all secrets in a certain mount. """
+
 import os
-import sys
 import hvac
 import urllib3
 
@@ -20,14 +25,14 @@ def get_token():
         return token
 
 
-def get_mount_version(source_mount):
-    secret_backend_config=client.sys.read_mount_configuration(source_mount)
+def get_mount_version(mount):
+    secret_backend_config=client.sys.read_mount_configuration(mount)
     if 'options' in secret_backend_config.keys():
-        print('mount is v2')
+        print('mount version is 2')
         secret_version='v2'
     else:
         secret_version='v1'
-        print('mount is v1')
+        print('mount version is 1')
     return secret_version
 
 # Initialize the client using TLS
@@ -38,25 +43,25 @@ client = hvac.Client(url='https://vault.dal.myhrtg.net:8200', token=os.environ["
 
 
 # This function will list all secrets in mount
-def list_secrets_in_path(mount_version, source_mount, dir_in_mount, final_secrets_list):
+def list_secrets_in_path(mount_version, mount, dir_in_mount, final_secrets_list):
     if '1' in mount_version:
-        objects_in_path = client.secrets.kv.v1.list_secrets(path=dir_in_mount, mount_point=source_mount)['data']['keys']
+        objects_in_path = client.secrets.kv.v1.list_secrets(path=dir_in_mount, mount_point=mount)['data']['keys']
     else:
-        objects_in_path = client.secrets.kv.v2.list_secrets(path=dir_in_mount, mount_point=source_mount)['data']['keys']
+        objects_in_path = client.secrets.kv.v2.list_secrets(path=dir_in_mount, mount_point=mount)['data']['keys']
     for object in objects_in_path:
         abs_path = '{}/{}'.format(dir_in_mount, object)
         if object.endswith('/'):
-            list_secrets_in_path(mount_version, source_mount, abs_path.strip('/'), final_secrets_list)
+            list_secrets_in_path(mount_version, mount, abs_path.strip('/'), final_secrets_list)
         else:
-            final_secrets_list.append('{}/{}'.format(source_mount, abs_path.lstrip(('/'))))
+            final_secrets_list.append('{}/{}'.format(mount, abs_path.lstrip(('/'))))
     return final_secrets_list
 
 
 def main():
-    source_mount = input("Enter the mount you wish to list: ")
-    mount_version = get_mount_version(source_mount)
+    mount = input("Enter the mount you wish to list: ")
+    mount_version = get_mount_version(mount)
     final_secrets_list = list()
-    original_mount_secrets = list_secrets_in_path(mount_version, source_mount, '', final_secrets_list)
+    original_mount_secrets = list_secrets_in_path(mount_version, mount, '', final_secrets_list)
     print(*original_mount_secrets, sep="\n")
 
 
